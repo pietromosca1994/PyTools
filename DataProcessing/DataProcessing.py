26f1f5121f22a03a0882df3a0a2854b034441570
pietromosca1994 <pietromosca1994@gmail.com>
2020-06-07 19:00:25 +0200

    subclassed ExtPdDataFrame with pandas.DataFrame

--- DataProcessing/DataProcessing.py
+++ DataProcessing/DataProcessing.py
@@ -1,99 +1,38 @@
 import pandas as pd
+import numpy as np
 import os
 import missingno as msno
 import seaborn as sn
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn import preprocessing    
+from sklearn import preprocessing
 
-class ExtPdDataFrame(object):
-    
-    def __init__(self, *args, **kwargs):  
-        '''
-        Initialisation method
-        '''
-        
-        if 'path' in kwargs:
-            self.path=kwargs['path']
-            self.load_data(kwargs['path'])
-        if 'dataframe' in kwargs:
-            self.dataframe=kwargs['dataframe']
-        if 'scaler' in kwargs:
-            self.scaler=kwargs['scaler']        
-        
-    def info(self):
-        '''
-        Displays information on the loaded dataframe
+class ExtPdDataFrame(pd.DataFrame):
+    def __init__(self, *args, **kwargs):    
+        super(ExtPdDataFrame, self).__init__(*args, **kwargs)
+        self.scaler=None
 
-        Returns
-        -------
-        None.
-
-        '''
-        self.dataframe.info()
-        self.miss_vis()
-        
-        return None
+    @property
+    def _constructor(self):
+        return type(self)
     
     def load_data(self, path):
         '''
-        Load data from a file 
-        
-        Returns
-        -------
-        None
-
+        Function to load data into a DataFrame from a file
         '''
-        self.path=path
         # get file name and file extension from path
-        filename, file_extension=os.path.splitext(self.path)
+        filename, file_extension=os.path.splitext(path)
         
         # load data based on format
         if file_extension=='.csv':
-            self.dataframe=pd.read_csv(self.path)            
-            print('[INFO] Loaded', self.path)
+            dataframe=self.__init__(pd.read_csv(path))            
+            print('[INFO] Loaded', path)
         #elif file_extension== 
         else:
-            raise NameError('Not a valid file path') 
-            
-        return None
+            raise NameError('Not a valid file path')
+        
+        return dataframe
     
-    
-    def miss_vis(self, *args, **kwargs):
-        '''
-        Visualise Missing Data statistics
-
-        Returns
-        -------
-        None.
-
-        '''
-        start_sample=kwargs.get('start_sample', 0)
-        end_sample=kwargs.get('end_sample', self.dataframe.shape[0])
-        
-        plt.figure()
-        msno.matrix(self.dataframe[start_sample:end_sample])
-        plt.show()
-        #msno.bar(self.dataframe[start_sample:end_sample])
-        
-        return None
-        
-    def corrMatrix_vis(self):
-        '''
-        Visualise Correlation Matrix
-
-        Returns
-        -------
-        None.
-            
-        '''
-        plt.figure()
-        self.corrMatrix=self.dataframe.corr()
-        sn.heatmap(self.corrMatrix, annot=True)
-        plt.show
-        
-        return None
-      
     def X_Y_split(self, X_labels, Y_labels):
         ''' 
         Method to split dataset in X and Y datasets
@@ -107,98 +46,108 @@ class ExtPdDataFrame(object):
 
         Returns
         -------
-        None.
+        X_dataframe
+        Y_dataframe
 
         '''
         
-        self.X=self.dataframe.get(X_labels)
-        self.Y=self.dataframe.get(Y_labels)
+        X_dataframe=self.get(X_labels)
+        Y_dataframe=self.get(Y_labels)
         
-        print('[INFO] Dataset splitted into X and Y sets')
-        
-        return None
+        return X_dataframe, Y_dataframe
     
     def train_test_split(self, *args, **kwargs):
         '''
-        Function to split train and test datasets
+        Wrpper for sklearn.model_selection.train_test_split
+        
         '''
         
-        self.X_train, self.X_test, self.Y_train, self.Y_test=train_test_split(self.X, self.Y, **kwargs)
-         
-        print('[INFO] Dataset splitted into Train (X_train, Y_train) and Test (X_test, Y_test) datasets')
+        training_dataframe, test_dataframe=train_test_split(self, *args, **kwargs)
         
-        return None 
-
-    def plot_2D(self, x_label, y_label, interval):
+        return training_dataframe, test_dataframe 
+    
+    def transform(self, transformation, *args, **kwargs):
         '''
-        Plots a 2D representation of 2 coloumns of self.dataframe
-        '''
+        Wrapper for sklearn.preprocessing
         
-        x=self.dataframe.get(x_label)
-        y=self.dataframe.get(y_label)
-        self.x_test=x
-        self.y_test=y
+        Parameters
+        ----------
+        transformation : 'string'
+            preprocessing transformation from sklearn.preprocessing
         
-        x=x[interval[0]: interval[1]]
-        y=y[interval[0]: interval[1]]
-                
-        plt.scatter(x,y)
-        plt.xlabel(x_label)
-        plt.ylabel(y_label)  
-        plt.show()
-        
-        return None
-         
-    def preprocess(self, transformation, *args, **kwargs):
-        '''
-        Function to preprocess the data.
-        sklearn.preprocessing wrapper
+        Returns
+        -------
+        self.scaler : scaler(object)
+            scaler
         '''
         
         if transformation=='StandardScaler': 
-            self.scaler=preprocessing.StandardScaler()
-            self.dataframe=pd.DataFrame(self.scaler.fit_transform(self.dataframe), columns=self.dataframe.columns)
-            print('[INFO] Executed StandardScaler on the dataset')
+            scaler=preprocessing.StandardScaler()
         
         elif transformation=='Normalizer':
-            self.scaler=preprocessing.Normalizer()
-            self.dataframe=pd.DataFrame(self.scaler.fit_transform(self.dataframe), columns=self.dataframe.columns)
-            print('[INFO] Executed Normalisation on the dataset')
+            scaler=preprocessing.Normalizer()
         
         elif transformation=='MinMaxScaler':
-            self.scaler=preprocessing.MinMaxScaler(*args, **kwargs)  
-            self.dataframe=pd.DataFrame(self.scaler.fit_transform(self.dataframe), columns=self.dataframe.columns)
-            print('[INFO] Executed MinMaxScaler on the dataset')
+            scaler=preprocessing.MinMaxScaler(*args, **kwargs)  
         
         elif transformation=='RobustScaler':
-            self.scaler=preprocessing.RobustScaler(*args, **kwargs)
-            self.dataframe=pd.DataFrame(self.scaler.fit_transform(self.dataframe), columns=self.dataframe)
-            print('[INFO] Executed RobustScaler on the dataset')
-             
-        else:
-            raise NameError('Transformation not implemented')     
+            scaler=preprocessing.RobustScaler(*args, **kwargs)
+        
+        
+        self.__init__(scaler.fit_transform(self.values), columns=self.columns)
+        self.scaler=scaler
         
         return self.scaler
         
     def inverse_transform(self, *args, **kwargs):
         '''
         Method to inverse the transformation applied to the data
-        '''
-        if 'scaler' in kwargs:
-            self.scaler=kwargs['scaler']
         
-        self.dataframe=pd.DataFrame(self.scaler.inverse_transform(self.dataframe), columns=self.dataframe.columns)
-        print('[INFO] Executed Inverse Transform on the dataset')
+        Parameters
+        ----------
+        transformation : 'string'
+            preprocessing transformation from sklearn.preprocessing
+        
+        Returns
+        -------
+        None      
+        '''
+        self.scaler=kwargs.get('scaler', self.scaler)
+        self.__init__(self.scaler.inverse_transform(self.values), columns=self.columns)
+        return None
+    
+    def miss_plt(self, *args, **kwargs):
+        '''
+        Visualise Missing Data statistics
+
+        Returns
+        -------
+        None.
+
+        '''
+        start_sample=kwargs.get('start_sample', 0)
+        end_sample=kwargs.get('end_sample', self.shape[0])
+        
+        plt.figure()
+        msno.matrix(self.dataframe[start_sample:end_sample])
+        plt.show()
+        #msno.bar(self.dataframe[start_sample:end_sample])
         
         return None
-         
-        
-        
-        
-        
-        
     
-        
-        
-        
+    def CorrMatrix_vis(self, *args, **kwargs):
+        '''
+        Visualise Correlation Matrix
+        Wrapper for pandas.DataFrame.corr()
 
+        Returns
+        -------
+        None.
+            
+        '''
+        plt.figure()
+        CorrMatrix=self.corr(*args, **kwargs)
+        sn.heatmap(CorrMatrix, annot=True)
+        plt.show
+        
+        return CorrMatrix
