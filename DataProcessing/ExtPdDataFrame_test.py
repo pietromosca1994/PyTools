aae76ea680b3e144707fee2b9a7289abeb919695
pietromosca1994 <pietromosca1994@gmail.com>
2020-06-11 20:15:40 +0200

    updated ExtPdDataFrame.py

--- DataProcessing/ExtPdDataFrame_test.py
+++ DataProcessing/ExtPdDataFrame_test.py
@@ -2,8 +2,8 @@ from DataProcessing import ExtPdDataFrame
 from skimage.util import view_as_windows
 import numpy as np
 
-X_features=['feature1', 'feature2']
-Y_features=['feature3']
+# Create ExtPdDataFrame
+columns=['feature1', 'feature2', 'feature3']
 data=np.array([[1,1,1],
                [2,2,2],
                [3,3,3],
@@ -13,15 +13,19 @@ data=np.array([[1,1,1],
                [7,7,7],
                [8,8,8],
                [9,9,9]])
-           
-columns=['feature1', 'feature2', 'feature3']
 data=ExtPdDataFrame(data, columns=columns)
+
+# split in X and Y dataframes
+X_features=['feature1', 'feature2']
+Y_features=['feature3']
 X_data, Y_data=data.X_Y_split(X_features, Y_features)
+
+# view as windows for RNN training
 window_length=2
 X_data.ViewAsWindows(window_length)
 dims=X_data.shape
-LSTM_X_data=np.reshape(X_data.values, (int(dims[0]/window_length), window_length, dims[1]))
-LSTM_Y_data=Y_data[window_length-1:]
+RNN_X_data=np.reshape(X_data.values, (int(dims[0]/window_length), window_length, dims[1]))
+RNN_Y_data=Y_data[window_length-1:]
 
 
 
